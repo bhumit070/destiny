499a083878a3a9ac46e1e92275e8ac9210676664
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-09 09:26:09 +0530

    add: code to parse directory name and chcek if given input is directory or not

--- src/main.go
+++ src/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("BEGINNING...")
+
+	args := os.Args[1:]
+
+	var directory string
+	if len(args) > 0 {
+		directory = args[0]
+		if directory == "" {
+			fmt.Println("No directory given")
+			os.Exit(1)
+		}
+	} else {
+		_directory, err := os.Getwd()
+
+		if err != nil {
+			panic(err)
+		}
+
+		directory = _directory
+	}
+
+	fileInfo, err := os.Stat(directory)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	if !fileInfo.IsDir() {
+		fmt.Println(directory, "is not a directory")
+		os.Exit(0)
+	}
+
 }
