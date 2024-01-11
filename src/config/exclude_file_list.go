99a3548cf0b5fa45a766bacee04eb23df4943fef
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-11 08:42:07 +0530

    add: code to move directory from one source to another

---
+++ src/config/exclude_file_list.go
@@ -0,0 +1,43 @@
+package config
+
+var ExcludedFileList = map[string]string{
+	// node related files
+	"package.json":      "package.json",
+	"package-lock.json": "package-lock.json",
+	"yarn.lock":         "yarn.lock",
+	"node_modules":      "node_modules",
+	"npm-debug.log":     "npm-debug.log",
+	"yarn-error.log":    "yarn-error.log",
+	"pnpm-debug.log":    "pnpm-debug.log",
+	"pnpm-lock.yaml":    "pnpm-lock.yaml",
+	"pnpmfile.js":       "pnpmfile.js",
+
+	// git
+	".gitignore": ".gitignore",
+
+	// zsh
+	".zshrc":       ".zshrc",
+	".zsh_history": ".zsh_history",
+	".zprofile":    ".zprofile",
+	".zcompdump":   ".zcompdump",
+
+	// bash
+	".bashrc":       ".bashrc",
+	".bash_history": ".bash_history",
+	".bash_profile": ".bash_profile",
+
+	// vim
+	".vimrc":   ".vimrc",
+	".viminfo": ".viminfo",
+	".vim":     ".vim",
+
+	// tmux
+	".tmux.conf":       ".tmux.conf",
+	".tmux.conf.local": ".tmux.conf.local",
+	".tmux":            ".tmux",
+	".tmuxinator":      ".tmuxinator",
+
+	// go
+	"go.mod": "go.mod",
+	"go.sum": "go.sum",
+}
--- src/main.go
+++ src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bhumit070/destiny/src/config"
 	"fmt"
 	"os"
+	"path"
+	"strings"
 )
 
 func main() {
@@ -36,6 +39,43 @@ func main() {
 	if !fileInfo.IsDir() {
 		fmt.Println(directory, "is not a directory")
 		os.Exit(0)
+	}
+
+	fileList, err := os.ReadDir(directory)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var directoryFileList map[string][]string = make(map[string][]string)
+
+	for _, file := range fileList {
+		if file.IsDir() {
+			continue
+		}
+		fileName := file.Name()
+
+		if config.ExcludedFileList[fileName] == fileName {
+			continue
+		}
+
+		splitFileName := strings.Split(fileName, ".")
+		fileExtension := splitFileName[len(splitFileName)-1]
+		directoryFileList[fileExtension] = append(directoryFileList[fileExtension], fileName)
+	}
+
+	for fileExtension, fileNames := range directoryFileList {
+		createDirectoryError := os.MkdirAll(fileExtension, 0755)
+		if createDirectoryError != nil {
+			fmt.Println(createDirectoryError.Error())
+			os.Exit(1)
+		}
+		for _, fileName := range fileNames {
+			source := path.Join(directory, fileName)
+			destination := path.Join(directory, fileExtension, fileName)
+			os.Rename(source, destination)
+		}
 	}
 
 }
