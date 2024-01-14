7ba31b7a2735263817738aeb8a222a3066f35006
bhumit070 <ganatrabhoomit070@gmail.com>
2024-01-14 08:07:12 +0530

    add: flag for folder group

--- src/config/valid_flags.go
+++ src/config/valid_flags.go
@@ -3,7 +3,24 @@ package config
 type InputFlags = map[string]string
 
 var ValidFlags = InputFlags{
-	"v": "v", // print version
-	"y": "y", // do not ask user confirmation
-	"q": "q", // do not print stats
+	"v":   "v",   // print version
+	"y":   "y",   // do not ask user confirmation
+	"q":   "q",   // do not print stats
+	"nfg": "nfg", // do not use folder groups
+}
+
+func IsValidFlag(flag string) bool {
+	if _, ok := ValidFlags[flag]; ok {
+		return true
+	}
+	return false
+}
+
+func IsFlagExists(flag string, flags *InputFlags) bool {
+	if _, ok := ValidFlags[flag]; ok {
+		if _, _ok := (*flags)[flag]; _ok {
+			return true
+		}
+	}
+	return false
 }
